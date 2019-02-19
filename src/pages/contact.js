caaf734294e3fb8c3913032e8078dd8117152e77
Athul CA <athul8720@gmail.com>
2019-02-19 20:58:23 +0530

    huhu...pooy settaaa....

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,7 +4,8 @@ import Header from '../components/header'
 export default()=>(
     <div style={{color:"Teal"}}>
     <Link to="/"> Home </Link>
-    <Header headerText="Even Exported Content"/>
+    <Header headerText="Pooy...Setta...."/>
     <img src="https://raw.githubusercontent.com/cek-open-source-club/Project-Libra/views/assets/shelf1.png" alt=""/> 
+    <p>Eda mwone Dence Keli.....</p>
     </div>
 )
