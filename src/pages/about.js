4ace01bb3ec8662a4f50564d903991484c618da8
Athul CA <athul8720@gmail.com>
2019-02-19 19:44:06 +0530

    duh.... Trying again

---
+++ src/pages/about.js
@@ -0,0 +1,9 @@
+import React from "react"
+import Header from '../components/header'
+
+export default()=>(
+    <div style={{color:'Teal'}}>
+    <Header headerText="Exported Text"/>
+    <p>Huhu...</p>
+    </div>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,8 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () => (<div style={{color:"purple",fontsize:"72px"}}>
+<h1>Yello..Yeloo..Yelooo....</h1>
+<p>Hello world Setta!</p>
+<img src="https://source.unsplash.com/random/400x200" alt=""/>
+</div>
+)
