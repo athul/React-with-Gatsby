fbf755a9b7294f20c28448845ef4c6b82aee60dd
Athul CA <athul8720@gmail.com>
2019-03-02 23:08:04 +0530

    add Aloy css modules

--- src/pages/about-css-modules.js
+++ src/pages/about-css-modules.js
@@ -1,9 +1,26 @@
-import React from 'react'
-import Container from '../components/container'
+import React from "react"
+import styles from "./about-css-modules.module.css"
+import Container from "../components/container"
 
-export default ()=>(
+console.log(styles)
+
+const User=props => (
+    <div className={styles.user}>
+        <img src={props.avatar} className={styles.avatar} alt=""/>
+        <div className={styles.description}>
+            <h2 className={styles.username}>{props.username}</h2>
+            <p classNmae={styles.excerpt}>{props.excerpt}</p>
+        </div>
+    </div>   
+)
+export default()=>(
     <Container>
-        <h1>HiHi CSS da</h1>
-        <p>Enna Summava</p>
+        <h1>About Aloy Annan</h1>
+        <p>Aloy Uyir</p>
+        <User 
+            username="Aloy Annan"
+            avatar="https://qph.fs.quoracdn.net/main-qimg-78ae97e270237bec39aa2f416adde6cc"
+            excerpt="i am Aloy .... Mess teste...theetham....."
+        />
     </Container>
 )
